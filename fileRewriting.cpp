676c2bfb535f6078c06a36677d8cd94f7514a9a0
Darrmar <a.sokolovskyy.02@outlook.com>
2020-12-17 19:00:22 +0200

    add to exam

--- fileRewriting.cpp
+++ fileRewriting.cpp
@@ -6,25 +6,58 @@ using namespace std;
 
 int main()
 {
-	ofstream input("input.txt");
-	input << "Change\n\n\n\n\nChange";
-	input.close();
-	const int size = 100;
-	char ina[size] = "";
-	char inb[size] = "";
+	fstream input("input.txt", ios::in);
+	fstream output("output.txt" , ios::out | ios::trunc);
+	int i;
+	char ina;
+	char inb;
+	string str;
+	char rep[80];
+	int count = 0;
 
 	cout << "old symbol " << endl;
 	cin >> ina;
 	cout << "new symbol " << endl;
 	cin >> inb;
-
-	input.open("input.txt", ios::out | ios::trunc);
-	input << inb;
+	
+	while (getline(input, str))
+	{
+		for (i = 0; i < str.length();i++){
+			if (str[i] == ina) {
+				rep[i] = inb;
+				count = count + 1;
+			}
+			else {
+				rep[i] = str[i];
+			}
+			output.put(rep[i]);
+		}
+		output << endl;
+	}
 	input.close();
+	output.close();
 
-	ofstream output("output.txt");
-	output << ina;
+	output.open("output.txt", ios::in);
+	input.open("input.txt", ios::out | ios::trunc);
 
+	while (getline(output, str))
+	{
+		for (i = 0; i < str.length(); i++) {
+			rep[i] = str[i];
+			input.put(rep[i]);
+		}
+		input << endl;
+	}
+
+	input.close();
+	output.close();
+
+	output.open("output.txt", ios::out | ios::trunc);
+
+	for (i = 0; i < count; i++) {
+		output << ina;
+	}
+	output.close();
 
 	system("pause");
 	return 0;
